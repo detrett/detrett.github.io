b312d6bedf8b20c3d4fdc35427d7b6a0fb813b5e
Alan <66321582+detrett@users.noreply.github.com>
2023-09-22 17:33:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+/* TOOLTIPS */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+/* BACK TO TOP BUTTON */
+var btn = $('#back-to-top-button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 700) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+/* GUEST/MEMBER SWITCH */
+var memberSwitch = $('#guest-switch');
+var guestSwitch = $('#member-switch');
+
+var memberToast = document.getElementById("member-toast")
+var guestToast = document.getElementById("guest-toast")
+
+var memberOnlyElements = Array.from (document.getElementsByClassName('member-only'));
+var guestOnlyElements = Array.from (document.getElementsByClassName('guest-only'));
+
+memberSwitch.on('click', function () {
+    console.log("Member switch pressed. You are now a guest");
+
+    /* Switch buttons */
+    guestSwitch.addClass('show');
+    memberSwitch.removeClass('show');
+
+    /* Toast */
+    const my_member_toast = bootstrap.Toast.getOrCreateInstance(guestToast);
+    my_member_toast.show();
+
+    /* Hide guest elements and show member elements */
+    
+    memberOnlyElements.forEach(el => {
+        $(el).addClass('hide');
+    })
+
+    guestOnlyElements.forEach(el => {
+        $(el).removeClass('hide');
+    })
+});
+
+guestSwitch.on('click', function () {
+    console.log("Guest switch pressed. You are now a member");
+
+    /* Switch buttons */
+    memberSwitch.addClass('show');
+    guestSwitch.removeClass('show');
+
+    /* Toast */
+    const my_guest_toast = bootstrap.Toast.getOrCreateInstance(memberToast);
+    my_guest_toast.show();
+
+    /* Hide member elements and show guest elements */
+
+    guestOnlyElements.forEach(el => {
+        $(el).addClass('hide');
+    })
+
+    memberOnlyElements.forEach(el => {
+        $(el).removeClass('hide');
+    })
+});
